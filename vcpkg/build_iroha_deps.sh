193b3294c1a8d39533e0c42460571a251309a402
Ivan 'kuvaldini' Kuvaldin <ivan@kuvaldini.pro>
2021-05-05 10:57:20 +0300

    [GHActions] WIP windows
    Signed-off-by: Ivan 'kuvaldini' Kuvaldin <ivan@kuvaldini.pro>

--- vcpkg/build_iroha_deps.sh
+++ vcpkg/build_iroha_deps.sh
@@ -8,37 +8,54 @@ VCPKG_COMMIT_SHA=${VCPKG_COMMIT_SHA:-da9defc3bddbba39edd9c7e04d4f2bc4bca3f6d4}
 build_dir=${2:-${build_dir:-$(dirname $script_dir)/build}}
 
 git -C $vcpkg_path fetch origin || 
-    git clone https://github.com/microsoft/vcpkg $vcpkg_path
+   git clone https://github.com/microsoft/vcpkg $vcpkg_path
 
 git -C $vcpkg_path checkout -f $VCPKG_COMMIT_SHA
 git -C $vcpkg_path/ports clean -fdx
 
 ## Apply patches, bypass if already applied, stop on fail
 for i in "$script_dir"/patches/*.patch; do 
-    if git -C $vcpkg_path apply --reverse --check --ignore-whitespace $i &>/dev/null
-        then continue ;fi
-    git -C $vcpkg_path apply --ignore-whitespace $i
+   if git -C $vcpkg_path apply --reverse --check --ignore-whitespace $i &>/dev/null
+      then continue ;fi
+   git -C $vcpkg_path apply --ignore-whitespace $i
 done
 
 ## maybe todo - simpler way to add and patch custom ports
 # cp -r "$script_dir"/ports $vcpkg_path/
 
+MANIFEST_ROOT=$(realpath $script_dir/..)
+BINARYSOURCE=files,$vcpkg_path/binarycache,readwrite
+INSTALL_ROOT=$vcpkg_path/installed
+
 ## Every time clean build of vcpkgtool takes 43 seconds on MacBook 2016 i7 2.8GHz
 ##                                 and takes 3 minutes on default GitHub runner
 ## ToDo try reuse existing vcpkg_tool
 case $(uname | tr '[:upper:]' '[:lower:]') in
-    windows*|msys*) $vcpkg_path/bootstrap-vcpkg.bat ;;
-    *) $vcpkg_path/bootstrap-vcpkg.sh -useSystemBinaries ;;
+   windows*|msys*) 
+      bootstrap(){
+         $vcpkg_path/bootstrap-vcpkg.bat
+      }
+      MANIFEST_ROOT=$(cygpath -wa $MANIFEST_ROOT)
+      BINARYSOURCE=$(cygpath -wa $BINARYSOURCE)
+      INSTALL_ROOT=$(cygpath -wa $INSTALL_ROOT)
+      ;;
+   *)
+      bootstrap(){
+         $vcpkg_path/bootstrap-vcpkg.sh -useSystemBinaries
+      }
+      ;;
 esac
+
+boostrap
 
 #todo use --x-manifest-root=$(git -C $script_dir rev-parse --show-toplevel)
 $vcpkg_path/vcpkg install \
-    --x-manifest-root=$(realpath $script_dir/..) \
-    --binarysource=files,$vcpkg_path/binarycache,readwrite \
-    --x-install-root=$vcpkg_path/installed \
+   --x-manifest-root=$MANIFEST_ROOT \
+   --binarysource=$BINARYSOURCE \
+   --x-install-root=$INSTALL_ROOT \
 
-#    --x-install-root=$build_dir/vcpkg_installed \
-#    --x-install-root=$vcpkg_path/installed \  #default for manifest mode is PWD/vcpkg_installed
+#   --x-install-root=$build_dir/vcpkg_installed \
+#   --x-install-root=$vcpkg_path/installed \  #default for manifest mode is PWD/vcpkg_installed
 
 ## Profiling
 ## system macos big sur on macbook pro 2016 i7 2.8GHz
